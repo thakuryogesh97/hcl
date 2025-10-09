567166b621aa4d7b84ebdd77e8e99124c3688837
thakuryogesh97 <pedagogy38@gmail.com>
2025-10-09 11:35:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 
 resource "aws_instance" "one" {
- count = 3
+ count = 2
   ami           = "ami-04c08fd8aa14af291"
   instance_type = "t3.micro"
   tags = {
-    Name = "prod-2-server-${count.index+1}"
+    Name = "prod-3-server-${count.index+1}"
   }
 }
